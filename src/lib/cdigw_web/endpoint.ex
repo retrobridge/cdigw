2ce516b3457e04aae458b554c684ccbdadc382b5
Michael Roach <git@c.mroach.com>
2020-04-26 20:19:52 +0200

    Add simple home page

--- src/lib/cdigw_web/endpoint.ex
+++ src/lib/cdigw_web/endpoint.ex
@@ -2,11 +2,16 @@ defmodule CdigwWeb.Endpoint do
   use Plug.Router
 
   plug Plug.Logger
+  plug Plug.Static, at: "/public", from: :cdigw
   plug :match
   plug :dispatch
 
   match "/~cddb/cddb.cgi" do
     CdigwWeb.CddbPlug.call(conn, %{})
+  end
+
+  match "/" do
+    send_file(conn, 200, "priv/static/index.html")
   end
 
   match _ do
