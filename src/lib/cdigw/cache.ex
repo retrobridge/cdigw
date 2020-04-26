6a717c2a51493761317e3cca17426074dba18131
Michael Roach <git@c.mroach.com>
2020-04-26 20:20:35 +0200

    add debugging

--- src/lib/cdigw/cache.ex
+++ src/lib/cdigw/cache.ex
@@ -3,16 +3,19 @@ defmodule Cdigw.Cache do
   Quick and dirty in-memory key-value store.
   """
   use Agent
+  require Logger
 
   def start_link(_) do
     Agent.start_link(fn -> %{} end, name: __MODULE__)
   end
 
   def get(key, default \\ nil) do
+    Logger.debug("cache get key=#{key}")
     Agent.get(__MODULE__, fn state -> Map.get(state, key, default) end)
   end
 
   def put(key, value) do
+    Logger.debug("cache put key=#{key}")
     Agent.update(__MODULE__, fn state -> Map.put(state, key, value) end)
   end
 end
