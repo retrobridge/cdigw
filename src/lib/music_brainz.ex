3d95e07dae8faecfbddb125c468ef8de4e0cb089
Michael Roach <git@c.mroach.com>
2020-04-27 20:58:53 +0200

    Add calculator for MusicBrainz Disc ID

--- src/lib/music_brainz.ex
+++ src/lib/music_brainz.ex
@@ -15,11 +15,53 @@ defmodule MusicBrainz do
 
   @sectors_per_second 75
   @default_inc ["artists", "recordings", "genres"]
+  @default_genre Cddb.default_genre()
 
   plug Tesla.Middleware.Logger
   plug Tesla.Middleware.BaseUrl, "http://musicbrainz.org/ws/2"
   plug Tesla.Middleware.JSON
   plug Tesla.Middleware.Headers, [{"user-agent", @user_agent}]
+
+  @doc """
+  Calculate the DiscID using the lead-out LBA and track LBAs.
+
+  There are 4 components that go into the hash:
+
+  * First track number (2-char hex)
+  * Last track number (2-char hex)
+  * Lead-out track LBA (8-char hex)
+  * 99 track LBAs (8-char hex, each)
+
+  When there are fewer than 99 tracks, `0` is the LBA to use.
+
+  The hex must be upper-case. The list is then hashed with *SHA-1* and
+  base64-encoded. MusicBrainz uses non-standard url-encoding. The mapping is:
+
+  * `+` => `.`
+  * `/` => `_`
+  * `=` => `-`
+
+  ### Example
+
+      iex> MusicBrainz.calculate_disc_id(205072, [150, 15239, 29625, 45763, 61420, 75862, 91642, 108918, 123698, 139895, 153589, 169239, 188495])
+      "J9dlQIdlpgKkwDn.nHo8SlQ0ks4-"
+  """
+  def calculate_disc_id(lead_out_lba, track_lbas) do
+    track_count = length(track_lbas)
+
+    track_lbas_hex = Enum.map(track_lbas, fn lba -> to_padded_hex(lba, 8) end)
+    padding = Enum.map(1..(99 - track_count), fn _ -> "00000000" end)
+
+    # we're assuming the first track is track is track 1 and the last track
+    # is the count of tracks
+    first_tnum_hex = "01"
+    last_tnum_hex = to_padded_hex(track_count, 2)
+    lead_out_lba_hex = to_padded_hex(lead_out_lba, 8)
+
+    data = [first_tnum_hex, last_tnum_hex, lead_out_lba_hex | track_lbas_hex ++ padding]
+
+    :crypto.hash(:sha, data) |> mb_url_encode64()
+  end
 
   @doc """
   Find a disc by its length in seconds and track LBA TOC
@@ -130,5 +172,19 @@ defmodule MusicBrainz do
       item when is_integer(item) -> item
       item when is_binary(item) -> String.to_integer(item)
     end)
+  end
+
+  # hex-encode a number and zero-pad it to the given `len`
+  defp to_padded_hex(number, len) do
+    number |> Integer.to_string(16) |> String.pad_leading(len, "0")
+  end
+
+  # base64-encode some data using the custom MusicBrainz url-encoding
+  defp mb_url_encode64(data) do
+    data
+    |> Base.encode64()
+    |> String.replace("+", ".")
+    |> String.replace("/", "_")
+    |> String.replace("=", "-")
   end
 end
